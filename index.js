c8b84e4e96a513fc99963370a82fbd04a83173c0
Jimmy <6257063+gymie@users.noreply.github.com>
2026-04-15 11:20:45 +0700

    tambah form CRUD mahasiswa

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 
+const bcrypt = require('bcrypt');
+
 const Database = require('better-sqlite3');
 const db = new Database('absensi.db');
 
@@ -34,7 +36,9 @@ app.get('/', (req, res) => {
   res.render('pages/index');
 });
 
-// Buat route untuk mata kuliah
+// ROUTE MATA KULIAH
+// ====================
+
 app.get('/mata-kuliah/list', (req, res) => {
   const mataKuliah = db.prepare('SELECT * FROM mata_kuliah').all();
 
@@ -75,6 +79,92 @@ app.post('/mata-kuliah/delete/:id', (req, res) => {
   stmt.run(id);
 
   res.redirect('/mata-kuliah/list');
+});
+
+
+// ROUTE MAHASISWA
+// ====================
+
+app.get('/mahasiswa/create', (req, res) => {
+  res.render('pages/mahasiswa/create');
+});
+
+app.get('/mahasiswa/list', (req, res) => {
+  const mahasiswa = db.prepare(`
+    SELECT mahasiswa.id, mahasiswa.nim, mahasiswa.program_studi, 
+      mahasiswa.angkatan, pengguna.nama, pengguna.email 
+    FROM mahasiswa
+    JOIN pengguna ON mahasiswa.pengguna_id = pengguna.id
+  `).all();
+
+  res.render('pages/mahasiswa/list', { mahasiswa });
+});
+
+app.get('/mahasiswa/edit/:id', (req, res) => {
+  const { id } = req.params;
+
+  const mahasiswa = db.prepare(`
+    SELECT mahasiswa.id, mahasiswa.nim, mahasiswa.program_studi, 
+      mahasiswa.angkatan, pengguna.nama, pengguna.email 
+    FROM mahasiswa
+    JOIN pengguna ON mahasiswa.pengguna_id = pengguna.id
+    where pengguna.id = ?
+  `).get(id);
+
+  res.render('pages/mahasiswa/edit', { mahasiswa });
+});
+
+app.post('/mahasiswa/create', (req, res) => {
+  // Ambil data dari form
+  const { nim, nama, email, program_studi, angkatan } = req.body;
+  
+  // simpan data mahasiswa ke tabel pengguna dahulu
+  const stmt = db.prepare(`
+    INSERT INTO pengguna (nama, email, password, peran) 
+    VALUES (?, ?, ?, ?)`);
+  result = stmt.run(nama, email, bcrypt.hashSync(nim, 10), 'mahasiswa');
+
+  // ambil id pengguna yang baru saja dibuat
+  const penggunaId = result.lastInsertRowid;
+
+  // simpan data mahasiswa ke tabel mahasiswa
+  const mahasiswaStmt = db.prepare(`
+    INSERT INTO mahasiswa (pengguna_id, nim, program_studi, angkatan) 
+    VALUES (?, ?, ?, ?)`);
+  mahasiswaStmt.run(penggunaId, nim, program_studi, angkatan);
+
+  res.redirect('/mahasiswa/list');
+});
+
+app.post('/mahasiswa/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const { nim, nama, email, program_studi, angkatan } = req.body;
+
+  // update data mahasiswa di tabel pengguna
+  const stmt = db.prepare(`
+    UPDATE pengguna 
+    SET nama = ?, email = ? 
+    WHERE id = ?`);
+  stmt.run(nama, email, id);
+
+  // update data mahasiswa di tabel mahasiswa
+  const mahasiswaStmt = db.prepare(`
+    UPDATE mahasiswa 
+    SET nim = ?, program_studi = ?, angkatan = ? 
+    WHERE pengguna_id = ?`);
+  mahasiswaStmt.run(nim, program_studi, angkatan, id);
+
+  res.redirect('/mahasiswa/list');
+});
+
+app.post('/mahasiswa/delete/:id', (req, res) => {
+  const { id } = req.params;
+
+  // hapus data mahasiswa di tabel pengguna
+  const stmt = db.prepare('DELETE FROM pengguna WHERE id = ?');
+  stmt.run(id);
+
+  res.redirect('/mahasiswa/list');
 });
 
 // jalankan server di port 3000
