dd1c93676cd6696c5fafceac3091f12b864e2684
Jimmy <6257063+gymie@users.noreply.github.com>
2026-04-15 17:52:44 +0700

    Implement CRUD functionality for Dosen and Admin, update Mahasiswa and Mata Kuliah views

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/mahasiswa/create', (req, res) => {
 
 app.get('/mahasiswa/list', (req, res) => {
   const mahasiswa = db.prepare(`
-    SELECT mahasiswa.id, mahasiswa.nim, mahasiswa.program_studi, 
+    SELECT pengguna.id, mahasiswa.nim, mahasiswa.program_studi, 
       mahasiswa.angkatan, pengguna.nama, pengguna.email 
     FROM mahasiswa
     JOIN pengguna ON mahasiswa.pengguna_id = pengguna.id
@@ -104,7 +104,7 @@ app.get('/mahasiswa/edit/:id', (req, res) => {
   const { id } = req.params;
 
   const mahasiswa = db.prepare(`
-    SELECT mahasiswa.id, mahasiswa.nim, mahasiswa.program_studi, 
+    SELECT pengguna.id, mahasiswa.nim, mahasiswa.program_studi, 
       mahasiswa.angkatan, pengguna.nama, pengguna.email 
     FROM mahasiswa
     JOIN pengguna ON mahasiswa.pengguna_id = pengguna.id
@@ -165,6 +165,154 @@ app.post('/mahasiswa/delete/:id', (req, res) => {
   stmt.run(id);
 
   res.redirect('/mahasiswa/list');
+});
+
+
+// ROUTE DOSEN
+// ====================
+
+app.get('/dosen/create', (req, res) => {
+  res.render('pages/dosen/create');
+});
+
+app.get('/dosen/list', (req, res) => {
+  const dosen = db.prepare(`
+    SELECT pengguna.id, dosen.nidn, dosen.departemen, pengguna.nama, pengguna.email 
+    FROM dosen
+    JOIN pengguna ON dosen.pengguna_id = pengguna.id
+  `).all();
+
+  res.render('pages/dosen/list', { dosen });
+});
+
+app.get('/dosen/edit/:id', (req, res) => {
+  const { id } = req.params;
+
+  const dosen = db.prepare(`
+    SELECT pengguna.id, dosen.nidn, dosen.departemen, pengguna.nama, pengguna.email
+    FROM dosen
+    JOIN pengguna ON dosen.pengguna_id = pengguna.id
+    where pengguna.id = ?
+  `).get(id);
+
+  res.render('pages/dosen/edit', { dosen });
+});
+
+app.post('/dosen/create', (req, res) => {
+  // Ambil data dari form
+  const { nidn, nama, email, departemen } = req.body;
+
+  // simpan data dosen ke tabel pengguna dahulu
+  const stmt = db.prepare(`
+    INSERT INTO pengguna (nama, email, password, peran) 
+    VALUES (?, ?, ?, ?)`);
+  result = stmt.run(nama, email, bcrypt.hashSync(nidn, 10), 'dosen');
+
+  // ambil id pengguna yang baru saja dibuat
+  const penggunaId = result.lastInsertRowid;
+
+  // simpan data dosen ke tabel dosen
+  const dosenStmt = db.prepare(`
+    INSERT INTO dosen (pengguna_id, nidn, departemen) 
+    VALUES (?, ?, ?)`);
+  dosenStmt.run(penggunaId, nidn, departemen);
+
+  res.redirect('/dosen/list');
+});
+
+app.post('/dosen/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const { nidn, nama, email, departemen } = req.body;
+
+  // update data dosen di tabel pengguna
+  const stmt = db.prepare(`
+    UPDATE pengguna
+    SET nama = ?, email = ?
+    WHERE id = ?`);
+  stmt.run(nama, email, id);
+
+  // update data dosen di tabel dosen
+  const dosenStmt = db.prepare(`
+    UPDATE dosen
+    SET nidn = ?,
+      departemen = ?
+    WHERE pengguna_id = ?`);
+  dosenStmt.run(nidn, departemen, id);
+
+  res.redirect('/dosen/list');
+});
+
+app.post('/dosen/delete/:id', (req, res) => {
+  const { id } = req.params;
+
+  // hapus data dosen di tabel pengguna
+  const stmt = db.prepare('DELETE FROM pengguna WHERE id = ?');
+  stmt.run(id);
+
+  res.redirect('/dosen/list');
+});
+
+
+// ROUTE ADMIN
+// ====================
+app.get('/admin/create', (req, res) => {
+  res.render('pages/admin/create');
+});
+
+app.get('/admin/list', (req, res) => {
+  const admin = db.prepare(`
+    SELECT id, nama, email 
+    FROM pengguna 
+    WHERE peran = 'admin'
+  `).all(); 
+
+  res.render('pages/admin/list', { admin });
+});
+
+app.get('/admin/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const admin = db.prepare(`
+    SELECT id, nama, email
+    FROM pengguna
+    WHERE id = ?
+  `).get(id);
+
+
+  res.render('pages/admin/edit', { admin });
+});
+
+app.post('/admin/create', (req, res) => {
+  const { nama, email } = req.body;
+
+  const stmt = db.prepare(`
+    INSERT INTO pengguna (nama, email, password, peran)
+    VALUES (?, ?, ?, ?)`);
+  stmt.run(nama, email, bcrypt.hashSync(email, 10), 'admin');
+
+  res.redirect('/admin/list');
+});
+
+app.post('/admin/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const { nama, email } = req.body;
+
+  const stmt = db.prepare(`
+    UPDATE pengguna
+    SET nama = ?, email = ?
+    WHERE id = ?`);
+  stmt.run(nama, email, id);
+  
+  res.redirect('/admin/list');
+});
+
+app.post('/admin/delete/:id', (req, res) => {
+  const { id } = req.params;
+
+  const stmt = db.prepare('DELETE FROM pengguna WHERE id = ?');
+  stmt.run(id);
+
+
+  res.redirect('/admin/list');
 });
 
 // jalankan server di port 3000
